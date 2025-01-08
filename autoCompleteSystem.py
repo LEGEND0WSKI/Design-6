c0ef352ccb40e66fc43f3f8effa514fc78ae53f6
LEGEND0WSKI <siddhesh.koli32@gmail.com>
2025-01-07 22:44:43 -0600

    autoComplete Issue

--- autoCompleteSystem.py
+++ autoCompleteSystem.py
@@ -1,4 +1,67 @@
 # Time:
 # Space:
-# Leetcode:
-# Issues:
+# Leetcode: No
+# Issues: Prirority queue logic for max heap issue
+
+from collections import defaultdict
+import heapq
+class AutocompleteSystem:
+
+    class TrieNode:
+
+        def __init__(self):
+            self.children = {}
+            self.startsWith = []  
+
+    def __init__(self, sentences: List[str], times: List[int]):
+        self.hmap = defaultdict(int)
+        self.search = ""
+        self.root = self.TrieNode()
+        
+        for i in range(len(sentences)):
+            sentence = sentences[i]
+            time = times[i]
+            self.hmap[sentence] += time
+            self.insert(sentence)
+        
+    def insert(self,word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = self.TrieNode()
+            curr = curr.children[c]
+            curr.startsWith.append(word)
+
+    def searchPrefix(self, prefix):
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return []
+            curr = curr.children[c]
+        return curr.startsWith                
+    
+
+    def input(self, c: str) -> List[str]:
+        if c == '#':
+            if self.search not in self.hmap:
+                self.insert(self.search)
+      
+            self.hmap[self.search] += 1
+            self.search = ""
+            # return []
+
+            self.search += c
+            lst = self.searchPrefix(self.search)              # list of candidates
+            
+            
+            pq = []
+            for sentence in lst:
+                heapq.heappush(pq, sentence) #issue here
+                if len(pq) >3:
+                    heapq.heappop(pq)
+
+            result = []
+            while pq:
+                result.append(heapq.heappop(pq))
+            return result[::-1]
+
